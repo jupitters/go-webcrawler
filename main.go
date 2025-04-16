ef9e3cc77bf03658dcbbaf9ea1396eab3e68a182
jupitters <sampaiobale@gmail.com>
2025-04-16 19:50:57 -0400

    getting html

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
+
+func getHTML(rawURL string) (string, error) {
+	resp, err := http.Get(rawURL)
+	if err != nil {
+		return "", err
+	}
+	body, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode > 399 {
+		return "", errors.New("resposta falhou com status 4XX ou 5XX")
+	}
+	
+	if resp.Header.Get("content-type") != "text/html" {
+		return "", errors.New("Content-Type not text/html")
+	}
+	
+	return string(body), nil
+}
 
 func main() {
 	args := os.Args
@@ -19,4 +44,10 @@ func main() {
 	}
 	
 	fmt.Printf("Iniciando crawler em: %s\n", args[1])
+	html, err := getHTML(args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(html)
 }
